7cf467a51425aea694e8ca2efb8e90bed349749d
Shrinath Wadkar <shrinath.wadkar@qt.io>
2024-01-07 14:28:31 +0530

    update setup scripts

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 
 cd ~
 
+echo 'Adding basrc and profiles..'
 mv .bashrc .bashrc_bkp
 ln -s dotfiles/bash/.bashrc .bashrc
+mv .profile .profile_bkp
 ln -s dotfiles/bash/.profile .profile
 
+echo 'Add exports and aliases...'
+cp dotfiles/bash/bash_exports.sh dotfiles/bash/bash_aliases.sh .
+echo 'Settingup vim...'
 chmod +x dotfiles/vim/pluginsetup.sh && dotfiles/vim/pluginsetup.sh
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
--- vim/pluginsetup.sh
+++ vim/pluginsetup.sh
@@ -1,19 +1,24 @@
-cd ~/.vim/
 
+echo 'Adding vimrc..'
+mv ~/.vimrc ~/.vimrc_bkp
+ln -s ~/dotfiles/vim/.vimrc .vimrc
+
+cd ~/.vim
+echo 'Installing pathogen...'
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-mkdir autoload
+echo 'Installing plugins..'
+mkdir -p ~/.vim/bundle && cd ~/.vim/bundle
 
 git clone https://github.com/vim-scripts/OmniCppComplete
 git clone https://github.com/morhetz/gruvbox.git
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/rking/ag.vim.git
 git clone https://github.com/peterhoeg/vim-qml
-git clone https://github.com/ludokng/vim-odyssey.git
 git clone https://github.com/preservim/nerdtree.git
 git clone https://github.com/kergoth/vim-bitbake.git
-git clone git://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/altercation/vim-colors-solarized.git
 git clone https://github.com/peterhoeg/vim-qml
 git clone https://github.com/ntpeters/vim-better-whitespace.git
 git clone https://github.com/valloric/youcompleteme
