7cd439d6bb9b66605b8b05f66ba228981fbd4f8d
Shrinath Wadkar <shrinath.wadkar@qt.io>
2024-01-07 14:38:36 +0530

    Remove Omnicomplete as the support is broken

--- vim/pluginsetup.sh
+++ vim/pluginsetup.sh
@@ -11,7 +11,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo 'Installing plugins..'
 mkdir -p ~/.vim/bundle && cd ~/.vim/bundle
 
-git clone https://github.com/vim-scripts/OmniCppComplete
 git clone https://github.com/morhetz/gruvbox.git
 git clone https://github.com/vim-airline/vim-airline
 git clone https://github.com/rking/ag.vim.git
